be4715adb67d30aae71381836b47cab944d90203
chiharakenta <chiharakenta0519@gmail.com>
2020-10-03 15:59:06 +0900

    create user model

---
+++ api/models/user.js
@@ -0,0 +1,37 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class user extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  user.init({
+    name: DataTypes.STRING,
+    icon: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    profile: DataTypes.TEXT,
+    url: DataTypes.STRING,
+    twitter_id: DataTypes.STRING,
+    facebook_id: DataTypes.STRING,
+    instagram_id: DataTypes.STRING,
+    github_id: DataTypes.STRING,
+    interesting_category_id: DataTypes.INTEGER,
+    introduced_code: DataTypes.STRING,
+    access_token: DataTypes.STRING,
+    deleted_at: DataTypes.DATE
+  }, {
+    sequelize,
+    modelName: 'user',
+    underscored: true,
+  });
+  return user;
+};
