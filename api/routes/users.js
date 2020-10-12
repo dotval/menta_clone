71f5b2ae3a3194e9401a67bbbad45383d4ca8e95
tadashimasuda <tmasu4260@gmail.com>
2020-10-12 15:42:17 +0900

    create users/userId function

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,9 +1,16 @@
 var express = require('express');
-var router = express.Router();
+var router = express.Router();48
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+// /users/userId
+router.get('/:id', function(req, res, next) {
+  
+  res.json(req.params.id); 
+});
+
 
 module.exports = router;
